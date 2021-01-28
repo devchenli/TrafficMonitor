d965eddf642b011ecb0319084ff4061403d2779f
devchenli <windowsdev@163.com>
2021-01-28 18:26:00 +0800

    1.28-18:25

--- TrafficMonitor.cpp
+++ TrafficMonitor.cpp
@@ -22,8 +22,6 @@ CTrafficMonitorApp::CTrafficMonitorApp()
 {
 	self = this;
 	// 支持重新启动管理器
-	//m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_RESTART;
-
 	// TODO: 在此处添加构造代码，
 	// 将所有重要的初始化放置在 InitInstance 中
 	CRASHREPORT::StartCrashReport();
